8c5fcebf0927e5f68dd5906b158e3340a7a4dd05
salkhaliefah <137141135+salkhaliefah@users.noreply.github.com>
2023-11-06 12:47:55 -0800

    innitial  commit

---
+++ src/Exam.java
@@ -0,0 +1,16 @@
+
+public class Exam
+{
+
+	public Exam()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+ System.out.println("safa");
+	}
+
+}
