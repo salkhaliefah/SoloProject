992f5d1538d9004d221edf93c15305eafe6c985a
salkhaliefah <137141135+salkhaliefah@users.noreply.github.com>
2023-11-22 23:05:51 -0800

    initial commit

---
+++ src/ExamSystem.java
@@ -0,0 +1,179 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+
+
+public class ExamSystem extends JFrame implements ActionListener
+{
+	
+	private JPanel panel;
+	JButton startButton;
+	private JTextArea examTextArea;
+	
+	private JPanel optionsPanel;
+	private int currentQuestion = 1;
+	
+
+	
+	public ExamSystem() 
+	{
+		setTitle("Exam System");
+		setSize(900, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		buildPanel();
+		add(panel);
+		setVisible(true);
+		
+		
+	}
+	
+	
+	private void buildPanel() 
+	{
+		panel = new JPanel(new BorderLayout());
+		
+		examTextArea = new JTextArea();
+		examTextArea.setEditable(false);
+		panel.add(examTextArea, BorderLayout.CENTER);
+		
+		
+		startButton = new JButton("Start Exam");
+		startButton.addActionListener(this);
+		panel.add(startButton, BorderLayout.SOUTH);
+		startButton.setPreferredSize(new Dimension(120, 40));
+		startButton.setFont(new Font("Arial", Font.PLAIN, 16));
+		
+		optionsPanel = new JPanel(new GridLayout(4,1)); 
+		panel.add(optionsPanel, BorderLayout.EAST);
+		optionsPanel.setVisible(false);
+		
+		
+		
+		
+	}
+	private void startExam() 
+	{
+		currentQuestion = 1;
+		displayQuestion();
+		showOptions();
+	}
+	
+	private void displayQuestion() {
+		
+		
+		if(currentQuestion == 1) {
+			examTextArea.setFont(new Font("Arial", Font.ITALIC, 25));
+			examTextArea.setText("LO 1: Which of the following variable names use the correct style?\n\n " +
+				 "A. TotalAmount\n "+
+				 "B. totalAmount\n" +
+				 "C. total_Amount\n"+
+				 "D. totalamount\n");
+			
+			
+		}else if (currentQuestion == 2) {
+			examTextArea.setText("LO 2: What is the purpose of the base case in recursion?\n\n"+
+					 "A. ?????\n"+
+					 "B. ?????\n"+
+					 "C. ?????\n"+
+					 "D. ?????? \n");
+			
+		}
+		
+	
+	
+
+}
+	
+	private void showOptions() 
+	{
+		optionsPanel.removeAll();
+		
+		JButton OA = new JButton("A");
+		JButton OB = new JButton("B");
+		JButton OC = new JButton("C");
+		JButton OD = new JButton("D");
+		
+		OA.addActionListener(this:: checkAnswer);
+		OB.addActionListener(this:: checkAnswer);
+		OC.addActionListener(this:: checkAnswer);
+		OD.addActionListener(this:: checkAnswer);
+		
+		optionsPanel.add(OA);
+		optionsPanel.add(OB);
+		optionsPanel.add(OD);
+		optionsPanel.add(OD);
+		
+		optionsPanel.add(OA);
+		optionsPanel.add(OB);
+		optionsPanel.add(OC);
+		optionsPanel.add(OD);
+		
+		
+		
+		
+		optionsPanel.setVisible(true);
+		validate();
+		
+		
+	}
+	
+	private void checkAnswer(ActionEvent e) 
+	{
+		String selectedOption = ((JButton) e.getSource()).getText();
+		String correctOption = getCorrectOption();
+		
+		if(selectedOption.equals(correctOption)) {
+			currentQuestion++;
+			
+			if(currentQuestion <= 2) {
+				displayQuestion();
+				showOptions();
+				
+				
+			}else {
+				examTextArea.setText("Exam completed, Congratulations!");
+				optionsPanel.setVisible(false);
+				validate();
+				
+			}
+			
+		}else {
+			examTextArea.setText("Incorrect answer, Please try again.");
+			
+		}
+	
+	}
+	
+	private String getCorrectOption() {
+		if (currentQuestion == 1) {
+			return "B" ;
+		}else if(currentQuestion == 2){
+			return "A";
+		}
+		return "";
+	}
+
+@Override
+public void actionPerformed(ActionEvent e) 
+{
+	if(e.getSource() == startButton)
+	{
+		startExam();
+	}
+}
+
+public static void main(String[] args)
+{
+	SwingUtilities.invokeLater(() -> {
+		new ExamSystem();
+});
+	
+	}
+}
+
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		LaunchPage launchPage = new LaunchPage();
+		ExamSystem ExamGUI = new ExamSystem();
 
 	}
 
