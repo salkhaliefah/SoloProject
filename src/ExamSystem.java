d1d1454f9a5ba8502166a2eafc8d29ff3381c050
salkhaliefah <137141135+salkhaliefah@users.noreply.github.com>
2023-11-24 15:47:46 -0800

    initail commit

--- src/ExamSystem.java
+++ src/ExamSystem.java
@@ -37,14 +37,16 @@ public class ExamSystem extends JFrame implements ActionListener
 	private void buildPanel() 
 	{
 		panel = new JPanel(new BorderLayout());
+		startButton = new JButton("Start Exam");
+		startButton.addActionListener(this);
+		optionsPanel = new JPanel(new GridLayout(4,1)); 
 		
 		examTextArea = new JTextArea();
 		examTextArea.setEditable(false);
 		panel.add(examTextArea, BorderLayout.CENTER);
 		
 		
-		startButton = new JButton("Start Exam");
-		startButton.addActionListener(this);
+
 		panel.add(startButton, BorderLayout.SOUTH);
 		startButton.setPreferredSize(new Dimension(120, 40));
 		startButton.setFont(new Font("Arial", Font.PLAIN, 16));
@@ -69,20 +71,17 @@ public class ExamSystem extends JFrame implements ActionListener
 		
 		if(currentQuestion == 1) {
 			examTextArea.setFont(new Font("Arial", Font.ITALIC, 25));
-			examTextArea.setText("LO 1: Which of the following variable names use the correct style?\n\n " +
-				 "A. TotalAmount\n "+
-				 "B. totalAmount\n" +
+			examTextArea.setText("LO 1: Which of the following variable names use the correct style?\n\n " + "A. TotalAmount\n "+ "B. totalAmount\n" +
 				 "C. total_Amount\n"+
 				 "D. totalamount\n");
 			
 			
 		}else if (currentQuestion == 2) {
-			examTextArea.setText("LO 2: What is the purpose of the base case in recursion?\n\n"+
-					 "A. ?????\n"+
-					 "B. ?????\n"+
+			examTextArea.setText("LO 2: What is the purpose of the base case in recursion?\n\n" + "A. ?????\n" +  "B. ?????\n"+
 					 "C. ?????\n"+
 					 "D. ?????? \n");
 			
+			//questions 3,4,5,6,7,8,9,10>>>>>
 		}
 		
 	
@@ -95,24 +94,22 @@ public class ExamSystem extends JFrame implements ActionListener
 		optionsPanel.removeAll();
 		
 		JButton OA = new JButton("A");
-		JButton OB = new JButton("B");
-		JButton OC = new JButton("C");
-		JButton OD = new JButton("D");
-		
 		OA.addActionListener(this:: checkAnswer);
+		JButton OB = new JButton("B");
 		OB.addActionListener(this:: checkAnswer);
+		JButton OC = new JButton("C");
 		OC.addActionListener(this:: checkAnswer);
+		JButton OD = new JButton("D");
 		OD.addActionListener(this:: checkAnswer);
 		
-		optionsPanel.add(OA);
-		optionsPanel.add(OB);
-		optionsPanel.add(OD);
-		optionsPanel.add(OD);
+		
 		
 		optionsPanel.add(OA);
 		optionsPanel.add(OB);
 		optionsPanel.add(OC);
 		optionsPanel.add(OD);
+		
+
 		
 		
 		
@@ -131,7 +128,8 @@ public class ExamSystem extends JFrame implements ActionListener
 		if(selectedOption.equals(correctOption)) {
 			currentQuestion++;
 			
-			if(currentQuestion <= 2) {
+			if(currentQuestion <= 2) 
+			{
 				displayQuestion();
 				showOptions();
 				
@@ -143,14 +141,16 @@ public class ExamSystem extends JFrame implements ActionListener
 				
 			}
 			
-		}else {
+		}else 
+		{
 			examTextArea.setText("Incorrect answer, Please try again.");
 			
 		}
 	
 	}
 	
-	private String getCorrectOption() {
+	private String getCorrectOption() 
+	{
 		if (currentQuestion == 1) {
 			return "B" ;
 		}else if(currentQuestion == 2){
