10392f5be52940dad014a5846495090c68d80745
salkhaliefah <137141135+salkhaliefah@users.noreply.github.com>
2023-12-07 19:48:47 -0800

    commit
    added a few comments to code, yet to add more elements and comments

--- src/ExamSystem.java
+++ src/ExamSystem.java
@@ -84,114 +84,140 @@ public class ExamSystem extends JFrame implements ActionListener
 			examTextArea.setFont(new Font("Arial", Font.ITALIC, 25));
 			//Changing color of the window background 
 			examTextArea.setBackground(Color.lightGray);
-			examTextArea.setText("LO 1: Which of the following variable names use the correct style?\n\n " + "A. TotalAmount\n "+ "B. totalAmount\n" +
+			examTextArea.setText("LO 1: Which of the following variable names use the correct style?\n\n" + 
+			     
+					
+				 
+				 
+				 "A. TotalAmount\n "+
+				 "B. totalAmount\n" +
 				 "C. total_Amount\n"+
 				 "D. totalamount\n");
 			
 			
 		}else if (currentQuestion == 2) {
-			examTextArea.setText("LO2: Given: int[] a = {1, 2, 3}; What will be after; a[1] = 5?\n"
-					+ "A. {1, 5, 2, 3} \n"
-					+ "B. {5, 2, 3} \n"
-					+ "C. {1, 5, 3} \n"
-					+ "D. {5, 1, 2, 3} \n"
-					+ "");
+			examTextArea.setText("LO2: Given: int[] a = {1, 2, 3}; What will be after; a[1] = 5?\n\n"    +
+					
+					
+			         
+					
+					
+			         "A. {1, 5, 2, 3} \n"+
+					 "B. {5, 2, 3} \n" +
+					 "C. {1, 5, 3} \n" +
+					 "D. {5, 1, 2, 3} \n" +
+					 "");
 			
 	
 		}else if (currentQuestion == 3) {
-			examTextArea.setText("LO3: Which of the following is correct?\n"
-					+ "public class Student\n"
-					+ "{ \n"
-					+ "String name;\n"
-					+ " Address homeAddress;\n"
-					+ "}\n"
-					+ "public class Address\n"
-					+ "{\n"
-					+ "  int number;\n"
-					+ "  String streetName;\n"
-					+ "  String cityName;\n"
-					+ "  String stateName;\n"
-					+ "  int zipCode;\n"
-					+ "}\n"
-					+ "A. Student HAS-A Address \n"
-					+ "B. Student IS-A Address \n"
-					+ "C. Address HAS-A Student \n"
-					+ "D. Address IS-A Student \n"
-					+ "");
+			examTextArea.setText("\n LO3: Which of the following is correct?\n\n" + "public class Student\n" +
+					 "{ \n" +
+					 "String name;\n" +
+					 " Address homeAddress;\n" +
+					 "}\n\n" +
+					 "public class Address\n" +
+					 "{\n" +
+					 "  int number;\n" +
+					 "  String streetName;\n" +
+					 "  String cityName;\n" +
+					 "  String stateName;\n" +
+					 "  int zipCode;\n" +
+					 "}\n" +
+					 
+					 
+					 "A. Student HAS-A Address \n" +
+					 "B. Student IS-A Address \n" +
+					 "C. Address HAS-A Student \n" +
+					 "D. Address IS-A Student \n" +
+					 "");
 			
 			
 			
 		}else if(currentQuestion == 4) {
-			examTextArea.setText("LO4: What is a base class in inheritance?\n"
-					+ "A. Child class\n"
-					+ "B. Parents class\n"
-					+ "C. Derived class \n"
-					+ "D. Subclass\n"
-					+ "");
+			examTextArea.setText("LO4: What is a base class in inheritance?\n\n" +
+					
+					
+					 "A. Child class\n"+
+					 "B. Parents class\n"+
+					 "C. Derived class \n"+
+					 "D. Subclass\n"+
+					 "");
 			
 			
 		}else if (currentQuestion == 5) {
-			examTextArea.setText("LO5: What is polymorphism in java \n"
-					+ "A. Multiple inheritance\n"
-					+ "B. Code duplication \n"
-					+ "C. Code flexibility\n"
-					+ "D. Data hiding \n"
-					+ " ");
+			examTextArea.setText("LO5: What is polymorphism in java \n\n" +
+					
+					
+					 "A. Multiple inheritance\n"+
+					 "B. Code duplication \n"+
+					 "C. Code flexibility\n"+
+					 "D. Data hiding \n"+
+					 " ");
 			
 			
 			
 		}else if (currentQuestion == 6) {
-			examTextArea.setText("LO6: What is the primary purpose of a stack data structure?\n"
-					+ "A. To store date in sorted order \n"
-					+ "B. To provide quick access to the middle element\n"
-					+ "C. To manage data in a last-in, frist-out(LIFO)manner.\n"
-					+ "D. To facilitate random access to elements\n"
-					+ " ");
+			examTextArea.setText("LO6: What is the primary purpose of a stack data structure?\n\n" +
+					
+					
+					 "A. To store date in sorted order \n"+
+					 "B. To provide quick access to the middle element\n"+
+					 "C. To manage data in a last-in, frist-out(LIFO)manner.\n"+
+					 "D. To facilitate random access to elements\n"+
+					 " ");
 			
 			
 			
 		}else if (currentQuestion == 7) {
 			
-			examTextArea.setText("LO7: What does the term “polymorphism” mean in object-oriented design? \n"
-					+ "A. The ability of a classroom inherit from multiple classes\n"
-					+ "B. The ability to create objects of different types \n"
-					+ "C. The ability to write methods with the same name but different implementations. \n"
-					+ "D. The ability to hide the details of an object’s implementation\n"
-					+ " ");
+			examTextArea.setText("LO7: What does the term “polymorphism” mean in object-oriented design? \n\n" +
+					
+					
+					 "A. The ability of a classroom inherit from multiple classes\n"+
+					 "B. The ability to create objects of different types \n"+
+					 "C. The ability to write methods with the same name but different implementations. \n"+
+					 "D. The ability to hide the details of an object’s implementation\n"+
+					 " ");
 			
 			
 		}else if (currentQuestion == 8) {
-			examTextArea.setText("LO8: What does GUI stand for?\n"
-					+ "A. A visual component on the screen \n"
-					+ "B. An action or occurrence detected by the program \n"
-					+ "C. A graphical user interface \n"
-					+ "D. A type of layout manager\n"
-					+ " ");
+			examTextArea.setText("LO8: What does GUI stand for?\n\n" +
+					
+					
+					 "A. A visual component on the screen \n"+
+					 "B. An action or occurrence detected by the program \n"+
+					 "C. A graphical user interface \n"+
+					 "D. A type of layout manager\n"+
+					 " ");
 			
 			
 		}else if (currentQuestion == 9) {
-			examTextArea.setText("LO9: What is an exception in Java?\n"
-					+ "A. A regular program output \n"
-					+ "B. An error that occurs during program execution \n"
-					+ "C. A common programming data type \n"
-					+ "D. A standard data type \n"
-					+ " ");
+			examTextArea.setText("LO9: What is an exception in Java?\n\n" +
+					
+					
+					 "A. A regular program output \n"+
+					 "B. An error that occurs during program execution \n"+
+					 "C. A common programming data type \n"+
+					 "D. A standard data type \n"+
+					 " ");
 			
 			
 		}else if (currentQuestion == 10) {
-			examTextArea.setText("LO10: Which class in java is used for writing to a file?\n"
-					+ "A. FileWriter\n"
-					+ "B. FileReader\n"
-					+ "C.BufferedReader\n"
-					+ "D. PrintWriter\n"
-					+ "");
+			examTextArea.setText("LO10: Which class in java is used for writing to a file?\n\n" +
+					
+					
+					 "A. FileWriter\n"+
+					 "B. FileReader\n"+
+					 "C.BufferedReader\n"+
+					 "D. PrintWriter\n"+
+					 "");
 		}else if (currentQuestion == 11) {
-			examTextArea.setText("LO11: What is the purpose of the base case in recursion?\n"
-					+ "A. It’s just a placeholder\n"
-					+ "B. It helps with iteration\n"
-					+ "C. Its optional in recursion\n"
-					+ "D. it stops recursion \n"
-					+ "");
+			examTextArea.setText("LO11: What is the purpose of the base case in recursion?\n\n" +
+					 "A. It’s just a placeholder\n" +
+					 "B. It helps with iteration\n"+
+					 "C. Its optional in recursion\n"+
+					 "D. it stops recursion \n"+
+					 "");
 		}
 		
 		
